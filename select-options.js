a64c8ec5705734fcb8ca9bf57621398a428bcdaf
Johan Swan <53649803+JohanSwannie@users.noreply.github.com>
2023-01-11 14:58:04 +1100

    Update select-options.js

--- select-options.js
+++ select-options.js
@@ -1,12 +1,16 @@
-
 let houseSizes = {};
-houseSizes['HUGE'] = ['13 Bedrooms', '11 Bedrooms', '8 Bedrooms'];
-houseSizes['BIG'] = ['6 Bedrooms', '5 Bedrooms', '4 Bedrooms'];
+houseSizes['PALACE'] = ['150 Bedrooms', '120 Bedrooms', '100 Bedrooms'];
+houseSizes['HUGE'] = ['30 Bedrooms', '25 Bedrooms', '20 Bedrooms'];
+houseSizes['EXTRA LARGE'] = ['15 Bedrooms', '12 Bedrooms', '10 Bedrooms'];
+houseSizes['BIG'] = ['7 Bedrooms', '6 Bedrooms', '5 Bedrooms'];
 houseSizes['MEDIUM'] = ['4 Bedrooms', '3 Bedrooms', '2.5 Bedrooms'];
 houseSizes['SMALL'] = ['2 Bedrooms', '1 Bedroom'];
+houseSizes['TINY'] = ['1 Bedroom', 'half Bedroom'];
 
-function ChangeHouseList() {
-  let chosenHouse = house.options[house.selectedIndex].value;
+const housy = document.getElementById('house');
+
+function addHouseSizeList() {
+  let chosenHouse = housy.options[housy.selectedIndex].value;
   while (houseSize.options.length) {
     houseSize.remove(0);
   }
